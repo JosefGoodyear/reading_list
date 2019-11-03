894caf16e4aa964f889911694e0fd7100a1fd3b7
Josef Goodyear <josefgoodyear@gmail.com>
2019-11-03 10:28:35 -0800

    Format books to strings for printing

--- add_book.py
+++ add_book.py
@@ -6,7 +6,7 @@ def search(query):
         publisher information of the first 5 results """
     url = 'https://www.googleapis.com/books/v1/volumes?q={}'.format(query)
     obj = requests.get(url).json()
-    book_list = []
+    books = []
     item_range = min(obj.get('totalItems'), 5)
     if item_range == 0:
         print("Sorry, your search didn't return any results.")
@@ -16,11 +16,26 @@ def search(query):
         title = book_info.get('title')
         authors = book_info.get('authors')
         publisher = book_info.get('publisher')
-        book_list.append([title, authors, publisher])
-    return book_list
+        books.append([title, authors, publisher])
+    return books
     
+def format_books(books):
+    """ Format results into strings for printing """
+    formatted_books = []
+    for num, book in enumerate(books):
+        if book[1] is not None:
+            authors = ', '.join(book[1])
+        else:
+            authors = 'None'
+        formatted_books.append('{}: {} by {} ({})'.format(num + 1, book[0], authors, book[2]))
+    return formatted_books
+
 
 def add_book():
     """ Search, choose and add a book to your reading list """
     query = input("What book are you looking for?\n")
-    print(search(query))
+    books = search(query)
+    formatted_books = format_books(books)
+    print(formatted_books)
+
+   
--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest, read, view_reading_list, add_book
 
 class searchTest(unittest.TestCase):
-    
+
+
     def test_full_return_length(self):
         books = add_book.search('test')
         self.assertEqual(len(books), 5)
@@ -13,6 +14,20 @@ class searchTest(unittest.TestCase):
     def test_empty_return(self):
         with self.assertRaises(SystemExit):
             add_book.search('kf4ieraeroiujero2jlk')
+
+
+class formatBooksTest(unittest.TestCase):
+
+
+    def test_multiple_authors(self):
+        books = [['Fake Book', ['Author 1', 'Author 2'], 'Publisher']]
+        formatted_books = add_book.format_books(books)
+        self.assertEqual(formatted_books[0], '1: Fake Book by Author 1, Author 2 (Publisher)')
+    
+    def test_authors_is_none(self):
+        books = [['Fake Book', None, 'Publisher']]
+        formatted_books = add_book.format_books(books)
+        self.assertEqual(formatted_books[0], '1: Fake Book by None (Publisher)')
 
 
 if __name__ == '__main__':
