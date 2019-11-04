4e566375325b11ac58e183538b495b15eeb0d025
Josef Goodyear <josefgoodyear@gmail.com>
2019-11-04 09:15:41 -0800

    Add ability to view reading list

--- view_reading_list.py
+++ view_reading_list.py
@@ -1,2 +1,8 @@
 def view_reading_list():
-    print('View Your Reading List')
+    try:
+        with open('ReadingList.txt') as f:
+            reading_list = f.read()
+        print(reading_list)
+    except IOError:
+        print('Your reading list could not be found.' +
+              'Please name the file ReadingList.txt.')
