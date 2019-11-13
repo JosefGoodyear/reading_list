31bee88edd0754defa6ff1608f3a80bcc43adb5e
Josef Goodyear <josefgoodyear@gmail.com>
2019-11-12 16:08:12 -0800

    Add loop so that the program only exits with an explicit user command

--- read.py
+++ read.py
@@ -4,22 +4,28 @@ from ReadingList import ReadingList
 
 
 def main():
-    choice = input("Welcome to your reading list. Please choose an option:\n" +
-                   "1: Add a book\n2: View your reading list\n3: Exit\n")
-    if choice == '1':
-        obj = Book.get_books(input('What are you looking for?\n'))
-        books = []
-        for i in range(5):
-            books.append(Book(obj, i))
-            books[i].print_book()
-    elif choice == '2':
+    while True:
+        choice = input('Welcome to your reading list.' +
+                       ' Please choose an option:' +
+                       '\n1: Add a book\n2: View your reading list\n3: Exit\n')
         reading_list = ReadingList('ReadingList.txt')
-        reading_list.view()
-    elif choice == '3':
-        exit()
-    else:
-        print("That is not a valid choice. Please choose 1, 2, or 3.")
-        main()
+        if choice == '1':
+            obj = Book.get_books(input('What are you looking for?\n'))
+            books = []
+            for i in range(5):
+                books.append(Book(obj, i))
+                books[i].print_book()
+            book_to_add = input('Enter the corresponding number' +
+                                ' to add a book to your reading list' +
+                                ' or any other key to exit:\n')
+            if book_to_add.isdigit() and 0 < int(book_to_add) <= 5:
+                reading_list.add_book(books[int(book_to_add) - 1])
+        elif choice == '2':
+            reading_list.view()
+        elif choice == '3':
+            exit()
+        else:
+            print("That is not a valid choice. Please choose 1, 2, or 3.")
 
 
 if __name__ == '__main__':
