9cce27cf903f9415307ad5ee6270e880da858e0c
Josef Goodyear <josefgoodyear@gmail.com>
2019-11-13 17:05:06 -0800

    Remove old files and rename class files for python naming conventions

--- view_reading_list.py
+++
@@ -1,9 +0,0 @@
-def view_reading_list(reading_list_file):
-    """ Print your reading list to the terminal """
-    try:
-        with open(reading_list_file) as f:
-            reading_list = f.read()
-        print()
-        print(reading_list)
-    except IOError:
-        print('{} could not be found.'.format(reading_list_file))
