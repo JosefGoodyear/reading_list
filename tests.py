f28602d49b150fdeb5cdcad4c13701d30ae6d98d
Josef Goodyear <josefgoodyear@gmail.com>
2019-11-13 16:30:05 -0800

    Add tests for ReadingList class

--- tests.py
+++ tests.py
@@ -28,7 +28,7 @@ class getApiItemsTest(unittest.TestCase):
         self.assertFalse(get_api_items('kf4ierae132roiujmlero2jlk'))
 
 
-class BookTest(unittest.TestCase):
+class bookTest(unittest.TestCase):
 
     def setUp(self):
         info = {'title': 'Fake Book', 'authors': ['Fake Author'],
@@ -59,46 +59,26 @@ class BookTest(unittest.TestCase):
         self.assertEqual(mock_stdout.getvalue(), expected_output)
 
     def test_print_book(self):
-        self.assert_stdout(2, '\n1: Fake Book by Fake Author (Fake Publisher)\n')
+        self.assert_stdout(2, '\n1: Fake Book by Fake Author ' +
+                              '(Fake Publisher)\n')
 
 
-"""
-class printBooksTest(unittest.TestCase):
+class readingListTest(unittest.TestCase):
 
-    books = ["1: Python by Joseph Eddy Fontenrose (Biblo & Tannen Publishers)",
-             "2: Python for Kids by Jason R. Briggs (No Starch Press)",
-             "3: Learning Python by Mark Lutz (O'Reilly Media, Inc.)",
-             "4: Python by Chris Fehily (Peachpit Press)",
-             "5: Python for Everybody by Charles R. Severance (None)"]
-
-    def test_out_of_range_book(self):
-        with self.assertRaises(SystemExit):
-            with mock.patch('builtins.input', return_value='0'):
-                add_book.print_books(self.books)
-
-    def test_book_on_edge(self):
-        expected = "5: Python for Everybody by Charles R. Severance (None)"
-        with mock.patch('builtins.input', return_value='5'):
-            book = add_book.print_books(self.books)
-            self.assertEqual(book, expected)
-
-    def test_other_key(self):
-
-        with self.assertRaises(SystemExit):
-            with mock.patch('builtins.input', return_value='quit'):
-                add_book.print_books(self.books)
-
-
-class addBookToReadingListTest(unittest.TestCase):
-
-    book1 = "2: Python for Kids by Jason R. Briggs (No Starch Press)"
-    book2 = "4: Python by Chris Fehily (Peachpit Press)"
+    def setUp(self):
+        info = {'title': 'Fake Book', 'authors': ['Fake Author'],
+                'publisher': 'Fake Publisher', 'publishedDate': '2007'}
+        self.book = Book(info, 1)
+        self.reading_list = ReadingList('TestReadingList.txt')
 
     def tearDown(self):
         if os.path.exists('TestReadingList.txt'):
             os.remove('TestReadingList.txt')
 
-    def test_new_line_for_each_entry(self):
+    def test_init(self):
+        self.assertEqual(self.reading_list.filename, 'TestReadingList.txt')
+
+    def test_add_book(self):
 
         def find_file_length(f):
             print(len(f))
@@ -107,33 +87,32 @@ class addBookToReadingListTest(unittest.TestCase):
                     pass
             return i + 1
 
-        add_book.add_to_reading_list(self.book1, "TestReadingList.txt")
-        length_1 = find_file_length("TestReadingList.txt")
-        add_book.add_to_reading_list(self.book2, "TestReadingList.txt")
-        length_2 = find_file_length("TestReadingList.txt")
+        self.reading_list.add_book(self.book)
+        length_1 = find_file_length(self.reading_list.filename)
+        self.reading_list.add_book(self.book)
+        length_2 = find_file_length(self.reading_list.filename)
         self.assertEqual(length_1 + 1, length_2)
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def assert_stdout(self, n, expected_output, mock_stdout):
-        add_book.add_to_reading_list(self.book1, "path/to/file.txt")
+    def assert_stdout_add(self, n, expected_output, mock_stdout):
+        self.reading_list.filename = '/path/to/file.txt'
+        self.reading_list.add_book(self.book)
         self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_fail_to_write(self):
-        self.assert_stdout(2, ('There was an error opening/writing ' +
-                               'to the file. Python for Kids by Jason R. '
-                               'Briggs (No Starch Press) ' +
-                               'was not added to your reading list.\n'))
-
-
-class viewReadingListTest(unittest.TestCase):
+    def test_add_book_fail_to_write(self):
+        self.assert_stdout_add(2, ('There was an error opening/writing ' +
+                                   'to the file. Fake Book by Fake Author ' +
+                                   '(Fake Publisher) was not added to your ' +
+                                   'reading list.\n\n'))
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def assert_stdout(self, n, expected_output, mock_stdout):
-        view_reading_list('doesNotExist.txt')
+    def assert_stdout_view(self, n, expected_output, mock_stdout):
+        self.reading_list.filename = 'doesNotExist.txt'
+        self.reading_list.view()
         self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_fail_to_open(self):
-        self.assert_stdout(2, ('doesNotExist.txt could not be found.\n')) """
+    def test_view_fail(self):
+        self.assert_stdout_view(2, 'doesNotExist.txt could not be found.\n\n')
 
 
 if __name__ == '__main__':
